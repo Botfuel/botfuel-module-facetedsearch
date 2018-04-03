bebef97127feb862a6139db7b2e649bfeb935857
Canh <canh85@gmail.com>
2018-04-03 17:11:35 +0200

    refactor: add more tests

--- packages/sample-ecommerce/test-config.js
+++ packages/sample-ecommerce/test-config.js
@@ -1,8 +1,8 @@
-module.exports = { 
-  adapter: { 
-    name: 'test', 
-  }, 
-  logger: 'error', 
-  modules: ['botfuel-facetedsearch'], 
-  path: __dirname, 
+module.exports = {
+  adapter: {
+    name: 'test',
+  },
+  logger: 'error',
+  modules: ['botfuel-facetedsearch'],
+  path: __dirname,
 };
